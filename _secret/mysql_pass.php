59a5084130b2858d08d5e22920c9e11aa980cbfe
dispencermoore <dispencer.moore@gmail.com>
2020-06-10 14:56:12 -0700

    Possible liking fix.

--- _secret/mysql_pass.php
+++ _secret/mysql_pass.php
@@ -2,7 +2,7 @@
 
 // mysql credentials
 $username = "root";
-$password = "asa192526";
+$password = "";
 $hostname = "127.0.0.1";
 $database = "openair";
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,6 @@ function attachEvents() {
           }
         }
       });    
-    }
   });
   
   $('.resource-container').hover(
